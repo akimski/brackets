9897b2e908ec6da12493dd274624277abab48ddf
Akim <qlikovsky@gmail.com>
2021-03-11 17:42:15 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const open = [], close = [], arr = [];
+  for (let entry of bracketsConfig) {
+    open.push(entry[0]);
+    close.push(entry[1]);
+  }
+
+  for (let ch of str) {
+    if (arr.length > 0 && close.indexOf(ch) === open.indexOf(arr[arr.length - 1])) {
+        arr.pop(); 
+    } else if (open.includes(ch)) {
+        arr.push(ch);
+    } else {
+      return false;
+    }
+  }
+ 
+ return arr.length === 0;
 }
